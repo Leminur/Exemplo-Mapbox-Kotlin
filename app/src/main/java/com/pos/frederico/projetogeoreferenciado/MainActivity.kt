4b21b0cee2358b1f052d1fe8eb60629f4a73bac3
Frederico Fernandes <fredericofernandesdefaria@gmail.com>
2018-12-19 16:35:56 -0200

    1 Commit Inicial

---
+++ app/src/main/java/com/pos/frederico/projetogeoreferenciado/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.pos.frederico.projetogeoreferenciado
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
+import com.mapbox.mapboxsdk.annotations.MarkerViewOptions
+import com.mapbox.mapboxsdk.constants.Style
+import com.mapbox.mapboxsdk.maps.MapView
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Uso do token
+        Mapbox.getInstance(this, getString(R.string.string_acesso_token))
+        setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        //mapView = supportFragmentManager.findFragmentById(R.id.mapView) as MapView
+        mapView.onCreate(savedInstanceState)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        mapView.getMapAsync(){
+            it.setStyle(Style.TRAFFIC_NIGHT)
+            // Customize map with markers, polylines, etc.
+
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+
+    public override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+
+}
