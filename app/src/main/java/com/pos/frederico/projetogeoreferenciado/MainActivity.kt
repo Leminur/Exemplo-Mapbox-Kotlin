e0b4b5d7078108052b6707202c799edbb169b087
Frederico Fernandes <fredericofernandesdefaria@gmail.com>
2019-01-01 19:10:19 -0200

    Commit da criação do GPS

--- app/src/main/java/com/pos/frederico/projetogeoreferenciado/MainActivity.kt
+++ app/src/main/java/com/pos/frederico/projetogeoreferenciado/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.pos.frederico.projetogeoreferenciado
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
 
 import com.hlab.fabrevealmenu.listeners.OnFABMenuSelectedListener
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.constants.Style
+import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), OnFABMenuSelectedListener {
+class MainActivity : AppCompatActivity(), OnFABMenuSelectedListener, PermissionsListener {
     private lateinit var mapView: MapView
+    private var permissionsManager: PermissionsManager? = null
+    private lateinit var mapboxMap: MapboxMap
+    private var locationComponent: LocationComponent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +44,7 @@ class MainActivity : AppCompatActivity(), OnFABMenuSelectedListener {
         //Dados valores padrões do mapbox
         mapView.getMapAsync {
             it.setStyle(Style.TRAFFIC_DAY)
+            mapboxMap = it
 
 //            val iconFactory: IconFactory = IconFactory.getInstance(this@MainActivity)
 //            val icon: Icon = iconFactory.fromResource(R.drawable.mapbox_logo_icon)
@@ -67,10 +81,66 @@ class MainActivity : AppCompatActivity(), OnFABMenuSelectedListener {
         }
     }
 
-    // Funções de acesso dos botões do mapa
 
+    // Funções de acesso dos botões do mapa
     private fun fabGPSFuncao() {
-        Toast.makeText(this@MainActivity, "Passei função GPS", Toast.LENGTH_SHORT).show()
+        // Checar se as permissões foram habilitadas, e se não, fazer as requisições delas.
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            inicializaGPS()
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager!!.requestLocationPermissions(this)
+        }
+
+    }
+
+    // Habilita o modo GPS
+    @SuppressLint("MissingPermission")
+    private fun inicializaGPS() {
+        if (locationComponent == null || !locationComponent!!.isLocationComponentEnabled) {
+            val options: LocationComponentOptions = LocationComponentOptions.builder(this)
+                .trackingGesturesManagement(true)
+                .accuracyColor(ContextCompat.getColor(this, R.color.padrao_azul_menu_200))
+                .build()
+
+            // Setar uma instancia do componente
+            locationComponent = mapboxMap.locationComponent
+            locationComponent!!.activateLocationComponent(this, options)
+
+            // Enable to make component visible
+            locationComponent!!.isLocationComponentEnabled = true
+
+            // Set the component's camera mode
+            locationComponent!!.cameraMode = CameraMode.TRACKING
+            locationComponent!!.renderMode = RenderMode.COMPASS
+            fabGPS.setImageDrawable(ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_gps_fixed_24dp))
+        } else {
+            locationComponent!!.isLocationComponentEnabled = false
+            fabGPS.setImageDrawable(ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_gps_not_fixed_24dp))
+        }
+
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        permissionsManager?.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        //Inserir dados para avisar o usuário
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            fabGPSFuncao()
+        } else {
+            //Caso o usuário recusar as permissões, a função abaixo será ser executado
+            val snackbar = Snackbar.make(root_layout, R.string.permissao_explicacao, Snackbar.LENGTH_INDEFINITE)
+            snackbar.setAction("Fechar") {
+                snackbar.dismiss()
+            }
+            snackbar.show()
+        }
     }
 
     private fun menuMapa() {
